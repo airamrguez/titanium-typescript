02aa792a9bb766362a2f9d95c743072d367f9eb7
Marco Seguri <marco.seguri@server.fake>
2014-02-10 04:48:53 +0100

    Update Underscore.js type definitions to v1.5.2

--- References.ts
+++ References.ts
@@ -1,4 +1,4 @@
 /// <reference path="node.d.ts" />
-/// <reference path="underscore.d.ts" />
+/// <reference path="underscore-1.5.2.d.ts" />
 /// <reference path="underscore.string.d.ts" />
 /// <reference path="Generator.ts" />
